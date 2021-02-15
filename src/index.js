c01300529c968417896679b52d2cf0a177fdb27f
michaelwenk <michael.wenk@uni-jena.de>
2021-02-15 16:27:35 +0100

    added rest of export names in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-export * from './CorrelationManager';
-export * from './model/Correlation';
-export * from './model/Link';
+import Correlation from './model/Correlation';
+
+export * as CorrelationManager from './CorrelationManager';
+export * as Correlation from './model/Correlation';
+export * as Link from './model/Link';
 export * as CorrelationUtilities from './utilities/CorrelationUtilities';
 export * as GeneralUtilities from './utilities/GeneralUtilities';
 export * as ProtonsCountUtilities from './utilities/ProtonsCountUtilities';
