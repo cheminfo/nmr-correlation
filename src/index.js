c2ffceb7905a361287787e20e5a3c33b0579db38
michaelwenk <michael.wenk@uni-jena.de>
2021-02-15 16:40:55 +0100

    fix: use "default as ..."

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Correlation from './model/Correlation';
 
-export * as CorrelationManager from './CorrelationManager';
-export * as Correlation from './model/Correlation';
-export * as Link from './model/Link';
+export { default as CorrelationManager } from './CorrelationManager';
+export { default as Correlation } from './model/Correlation';
+export { default as Link } from './model/Link';
 export * as CorrelationUtilities from './utilities/CorrelationUtilities';
 export * as GeneralUtilities from './utilities/GeneralUtilities';
 export * as ProtonsCountUtilities from './utilities/ProtonsCountUtilities';
