6e5a926ea3284f50fcc50f369e22c29901620513
michaelwenk <michael.wenk@uni-jena.de>
2021-03-22 01:49:53 +0100

    fix: invalid access to signal property in sortCorrelations method

--- src/utilities/BuildUtilities.ts
+++ src/utilities/BuildUtilities.ts
@@ -495,19 +495,17 @@ function setLabels(correlations: Values): void {
 
 function sortCorrelations(correlations: Values): Values {
   const compare = (corr1: Correlation, corr2: Correlation) => {
-    if (corr1.pseudo === true && corr2.pseudo === true) {
-      return 0;
+    if (corr1.pseudo === false && corr2.pseudo === false) {
+      if (corr1.signal.delta < corr2.signal.delta) {
+        return -1;
+      } else if (corr1.signal.delta > corr2.signal.delta) {
+        return 1;
+      }
     }
-    if (
-      (corr1.pseudo === false && corr2.pseudo === true) ||
-      corr1.signal.delta < corr2.signal.delta
-    ) {
+    if (corr1.label['origin'] < corr2.label['origin']) {
       return -1;
     }
-    if (
-      (corr1.pseudo === true && corr2.pseudo === false) ||
-      corr1.signal.delta > corr2.signal.delta
-    ) {
+    if (corr1.label['origin'] > corr2.label['origin']) {
       return 1;
     }
     return 0;
