5948edff59ae761d75556a475dbffc0e24a1ae8c
michaelwenk <michael.wenk@uni-jena.de>
2021-04-19 23:20:59 +0200

    feat: added test for getCorrelationIndex

---
+++ src/utilities/general/__tests__/getCorrelationIndex.test.ts
@@ -0,0 +1,13 @@
+import { Correlation, Values } from '../../../types/index';
+import { buildCorrelation } from '../../correlation/buildCorrelation';
+import { getCorrelationIndex } from '../getCorrelationIndex';
+
+describe('getCorrelationIndex', () => {
+  const correlation1: Correlation = buildCorrelation({ id: 'id1' });
+  const correlation2: Correlation = buildCorrelation({ id: 'id2' });
+  const values: Values = [correlation1, correlation2];
+
+  it('getCorrelationIndex', () => {
+    expect(getCorrelationIndex(values, correlation2)).toStrictEqual(1);
+  });
+});
