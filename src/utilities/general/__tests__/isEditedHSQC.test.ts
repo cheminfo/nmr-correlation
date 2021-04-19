3c2b5967d8220219e443bdf4f05c4abc67767201
michaelwenk <michael.wenk@uni-jena.de>
2021-04-19 23:44:58 +0200

    feat: added test for isEditedHSQC

---
+++ src/utilities/general/__tests__/isEditedHSQC.test.ts
@@ -0,0 +1,33 @@
+import { Spectrum2D } from '../../../types/index';
+import { isEditedHSQC } from '../isEditedHSQC';
+
+describe('isEditedHSQC', () => {
+  const spectrum1: Spectrum2D = {
+    info: {
+      pulseSequence: 'hsqced',
+      nucleus: ['1H', '13C'],
+      dimension: 2,
+      isFid: false,
+      experiment: 'hsqc',
+    },
+    zones: { values: [] },
+    id: 'id1',
+  };
+  const spectrum2: Spectrum2D = {
+    info: {
+      pulseSequence: 'hsqc',
+      nucleus: ['1H', '13C'],
+      dimension: 2,
+      isFid: false,
+      experiment: 'hsqc',
+    },
+    zones: { values: [] },
+    id: 'id1',
+  };
+  it('test 1, true', () => {
+    expect(isEditedHSQC(spectrum1)).toStrictEqual(true);
+  });
+  it('test 2, false', () => {
+    expect(isEditedHSQC(spectrum2)).toStrictEqual(false);
+  });
+});
