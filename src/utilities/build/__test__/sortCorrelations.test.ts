d4ffe8653a20d5ca55e5bff399f2625e6b6e7703
michaelwenk <michael.wenk@uni-jena.de>
2021-04-20 14:57:26 +0200

    feat: improved test for sortCorrelations

--- src/utilities/build/__test__/sortCorrelations.test.ts
+++ src/utilities/build/__test__/sortCorrelations.test.ts
@@ -4,32 +4,32 @@ import { buildCorrelation } from '../../correlation/buildCorrelation';
 describe('sortCorrelations', () => {
   const correlation1 = buildCorrelation({
     atomType: 'C',
-    label: { origin: 'C2' },
+    label: { origin: 'C1' },
     signal: { delta: 30.2, id: 'id1' },
   });
   const correlation2 = buildCorrelation({
     atomType: 'C',
-    label: { origin: 'C1' },
-    signal: { delta: 10.7, id: 'id2' },
+    label: { origin: 'C2' },
+    signal: { delta: 40.7, id: 'id2' },
   });
   const correlation3 = buildCorrelation({
     atomType: 'H',
     label: { origin: 'H1' },
-    signal: { delta: 4.1, id: 'id3' },
+    signal: { delta: 1.0, id: 'id3' },
   });
   const correlation4 = buildCorrelation({
     atomType: 'H',
-    label: { origin: 'H4' },
+    label: { origin: 'H2' },
     signal: { delta: 2.1, id: 'id4' },
   });
   it('test', () => {
     expect(
       sortCorrelations([
-        correlation1,
-        correlation2,
         correlation3,
+        correlation2,
         correlation4,
+        correlation1,
       ]),
-    ).toStrictEqual([correlation2, correlation1, correlation4, correlation3]);
+    ).toStrictEqual([correlation1, correlation2, correlation3, correlation4]);
   });
 });
