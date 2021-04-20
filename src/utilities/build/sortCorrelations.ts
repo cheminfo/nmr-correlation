c82f858093d643c192b9f6a748dd3f434e933589
michaelwenk <michael.wenk@uni-jena.de>
2021-04-20 14:56:59 +0200

    fix: possible wrong order from sortCorrelations

--- src/utilities/build/sortCorrelations.ts
+++ src/utilities/build/sortCorrelations.ts
@@ -3,7 +3,32 @@ import { Values } from '../../types/correlation/values';
 import { getCorrelationsByAtomType } from '../general/getCorrelationsByAtomType';
 
 export function sortCorrelations(correlations: Values): Values {
-  const compare = (corr1: Correlation, corr2: Correlation) => {
+  const compareAtomTypes = (atomType1: string, atomType2: string) => {
+    // C and H highest priority
+    if (atomType1 === 'C') {
+      return -1;
+    }
+    if (atomType2 === 'C') {
+      return 1;
+    }
+    if (atomType1 === 'H') {
+      return -1;
+    }
+    if (atomType2 === 'H') {
+      return 1;
+    }
+    // alphabetical sort
+    if (atomType1 < atomType2) {
+      return -1;
+    }
+    if (atomType1 > atomType2) {
+      return 1;
+    }
+
+    return 0;
+  };
+
+  const compareCorrelations = (corr1: Correlation, corr2: Correlation) => {
     if (corr1.pseudo === false && corr2.pseudo === false) {
       if (corr1.signal.delta < corr2.signal.delta) {
         return -1;
@@ -24,12 +49,13 @@ export function sortCorrelations(correlations: Values): Values {
   const atomTypes = correlations
     .map((correlation) => correlation.atomType)
     .filter((atomType, i, a) => a.indexOf(atomType) === i);
+  atomTypes.sort(compareAtomTypes);
   atomTypes.forEach((atomType) => {
     const correlationsAtomType = getCorrelationsByAtomType(
       correlations,
       atomType,
     );
-    correlationsAtomType.sort(compare);
+    correlationsAtomType.sort(compareCorrelations);
     sortedCorrelations = sortedCorrelations.concat(correlationsAtomType);
   });
 
