65393f8dc9f66614bae579160552d91d510a4e5f
michaelwenk <michael.wenk@uni-jena.de>
2021-04-27 00:21:25 +0200

    chore: put optional previous correlation data into options

--- src/build/buildCorrelationData.ts
+++ src/build/buildCorrelationData.ts
@@ -1,6 +1,5 @@
 import { CorrelationData } from '../types/correlation/correlationData';
 import { Options } from '../types/correlation/options';
-import { Values } from '../types/correlation/values';
 import { Spectra } from '../types/spectrum/spectra';
 import { buildState } from '../utilities/build/buildState';
 import { buildValues } from '../utilities/build/buildValues';
@@ -8,9 +7,8 @@ import { buildValues } from '../utilities/build/buildValues';
 export function buildCorrelationData(
   spectra: Spectra,
   options: Options,
-  prevValues: Values = [],
 ): CorrelationData {
-  const { tolerance, mf = '' } = options;
+  const { tolerance, mf = '', values: prevValues = [] } = options;
   const values =
     spectra && tolerance ? buildValues(spectra, mf, tolerance, prevValues) : [];
 
--- src/types/correlation/options.d.ts
+++ src/types/correlation/options.d.ts
@@ -1,6 +1,9 @@
+import { Values } from '..';
+
 import { Tolerance } from './tolerance';
 
 export interface Options {
   tolerance: Tolerance;
   mf?: string;
+  values?: Values;
 }
