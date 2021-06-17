e8c5ec7ccce2d580badff3e59092d5075b86f733
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-06-17 15:58:09 +0200

    chore: use loop instead forEach in setAttachmentsAndP...

--- src/utilities/build/setAttachmentsAndProtonEquivalences.ts
+++ src/utilities/build/setAttachmentsAndProtonEquivalences.ts
@@ -13,44 +13,46 @@ export function setAttachmentsAndProtonEquivalences(
   correlations: Values,
 ): Values {
   // update attachment information between heavy atoms and protons via HSQC or HMQC
-  correlations.forEach((correlation) => {
+  for (const correlation of correlations) {
     // remove previous set attachments
     removeAttachments(correlation);
     // add attachments
-    correlation.link
-      .filter(
-        (link) =>
-          link.experimentType === 'hsqc' || link.experimentType === 'hmqc',
-      )
-      .forEach((link) => {
-        const otherAtomType = link.atomType[link.axis === 'x' ? 1 : 0];
-        link.match.forEach((matchIndex) => {
-          addAttachment(correlation, otherAtomType, matchIndex);
-        });
-      });
-  });
+    const linksFiltered = correlation.link.filter(
+      (link) =>
+        link.experimentType === 'hsqc' || link.experimentType === 'hmqc',
+    );
+
+    for (const link of linksFiltered) {
+      const otherAtomType = link.atomType[link.axis === 'x' ? 1 : 0];
+      for (let matchIndex of link.match) {
+        addAttachment(correlation, otherAtomType, matchIndex);
+      }
+    }
+  }
   // reset previously set proton equivalences and set new ones
   // check heavy atoms with an unambiguous protons count
-  correlations.forEach((correlation) => {
+  for (const correlation of correlations) {
     if (
       correlation.atomType !== 'H' &&
       correlation.protonsCount.length === 1 &&
       hasAttachmentAtomType(correlation, 'H')
     ) {
+      const { equivalence, protonsCount } = correlation;
+
       let equivalences = 1;
-      if (correlation.protonsCount[0] === 3) {
+      if (protonsCount[0] === 3) {
         equivalences = 3;
-      } else if (correlation.protonsCount[0] === 2) {
+      } else if (protonsCount[0] === 2) {
         equivalences = 2;
       }
       const sharedEquivalences =
-        (correlation.equivalence * equivalences) /
-        correlation.attachment.H.length;
-      correlation.attachment.H.forEach((attachedProtonIndex) => {
+        (equivalence * equivalences) / correlation.attachment.H.length;
+
+      for (const attachedProtonIndex of correlation.attachment.H) {
         correlations[attachedProtonIndex].equivalence = sharedEquivalences;
-      });
+      }
     }
-  });
+  }
 
   return correlations;
 }
